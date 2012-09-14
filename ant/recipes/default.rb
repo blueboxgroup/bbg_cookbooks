56bf71644c0e4cc7c99a1ec5f518dfff72789635
Sam Cooper <sam@chgworks.com>
2012-09-14 12:14:00 -0700

    updated ant cookbook

--- ant/recipes/default.rb
+++ ant/recipes/default.rb
@@ -1,28 +1,10 @@
-#
-# Cookbook Name:: ant
-# Recipe:: default
-#
-# Copyright 2010, Blue Box Group, LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 include_recipe "build-essential"
+include_recipe "openjdk"
 
-remote_file "/tmp/apache-ant-1.8.1-bin.tar.gz" do
+remote_file "/tmp/apache-ant-1.8.2-bin.tar.gz" do
   owner "root"
   group "root"
-  source "http://www.fightrice.com/mirrors/apache/ant/binaries/apache-ant-1.8.1-bin.tar.gz"
+  source "http://www.fightrice.com/mirrors/apache/ant/binaries/apache-ant-1.8.2-bin.tar.gz"
   mode "0644"
   backup false
   action :create_if_missing
@@ -33,8 +15,8 @@ script "install-ant" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  tar xzvf apache-ant-1.8.1-bin.tar.gz
-  mv apache-ant-1.8.1/ /opt/ant
+  tar xzvf apache-ant-1.8.2-bin.tar.gz
+  mv apache-ant-1.8.2/ /opt/ant
   EOH
   not_if "/usr/bin/test -d /opt/ant"
 end
