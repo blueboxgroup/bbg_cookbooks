e5491d9987bd11b29403ddb35195bdfefd1ed0ed
Sam Cooper <sam@chgworks.com>
2012-09-14 12:37:22 -0700

    updated the nodejs cookbook

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -1,12 +1,10 @@
 maintainer       "Blue Box Group, LLC"
-maintainer_email "support@blueboxgrp.com"
-license          "Apache v2.0"
-description      "Installs/Configures nodejs"
+maintainer_email "support@bluebox.net"
+license          "Apache 2.0"
+description      "Installs/Configures node.js"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
-
-%w{centos redhat}.each do |os|
-  supports os
-end
-
+version          "0.0.1"
 depends          "build-essential"
+
+provides "nodejs"
+provides "npm"
